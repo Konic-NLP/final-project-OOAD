a682f2badc97036fae51ab509a5c88c588dbafeb
SimonXW <xiwa3199@colorado.edu>
2022-04-27 20:16:26 -0600

    simon

--- apps/df_goods/urls.py
+++ apps/df_goods/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url('^$', views.index, name="index"),
     url('^list(\d+)_(\d+)_(\d+)/$', views.good_list, name="good_list"),
     url('^(\d+)/$', views.detail, name="detail"),
-    url(r'^search/', views.ordinary_search, name="ordinary_search"),  # 全文检索
+    url(r'^ordinary_search/', views.ordinary_search, name="ordinary_search"),  # 全文检索
 ]
