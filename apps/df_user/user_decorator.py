da1ae99bc22dd5b3164c63d063967726eda8179b
Sijia(Konic) Ge <94318777+Konic-NLP@users.noreply.github.com>
2022-04-27 23:27:34 -0600

    Update user_decorator.py

--- apps/df_user/user_decorator.py
+++ apps/df_user/user_decorator.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 
 
-# go to login page if not login
+# go to login page if not login, this is decorator to wrap up the function
 def login(func):
     def login_fun(request, *args, **kwargs):
         if 'user_id' in request.session:
